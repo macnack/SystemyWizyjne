c27ffd2465783dd11c8b0d9b4c8fa2871ef8abd5
Maciej <maciej.krupka@rad4m.com>
2023-03-17 11:47:19 +0100

    add distortion do img

--- Filtracja/kuwahara.py
+++ Filtracja/kuwahara.py
@@ -1,5 +1,12 @@
 import cv2 as cv
 
+
+def after_three_is_white(img):
+    for idx_col in range(img.shape[1]):
+        if not(idx_col % 3):
+            img[:, idx_col] = 255
+    return img
+
 def filterKuwahara(img, kernel_size):
     sub_kernel_size = int(kernel_size/2)
     border_img = cv.copyMakeBorder(img, kernel_size, kernel_size, kernel_size, kernel_size, cv.BORDER_CONSTANT)
@@ -21,8 +28,11 @@ def filterKuwahara(img, kernel_size):
 
 if __name__ == '__main__':
     img = cv.imread("../images/lenna_color.png", cv.IMREAD_GRAYSCALE)
+    img = after_three_is_white(img=img)
     imgKuwahara = filterKuwahara(img, 5)
-    cv.imshow('Kuwahara filter', imgKuwahara)
-    cv.imshow('Image', img)
-    cv.waitKey(5000)
-    cv.destroyAllWindows()
+    while True:
+        cv.imshow('Kuwahara filter', imgKuwahara)
+        cv.imshow('Image', img)
+        key = cv.waitKey(10)
+        if key == 27:
+            cv.destroyAllWindows()
