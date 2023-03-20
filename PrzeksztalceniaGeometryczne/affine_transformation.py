4a1d933ec42f566f4cc6f746e240385e63602509
Maciej <maciej.krupka@rad4m.com>
2023-03-20 22:23:02 +0100

    add Straightening road

--- PrzeksztalceniaGeometryczne/affine_transformation.py
+++ PrzeksztalceniaGeometryczne/affine_transformation.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2 as cv
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
+path = "../images/"
 # mouse callback function
 mouse_event = ['EVENT_MOUSEMOVE', 'EVENT_LBUTTONDOWN', 'EVENT_RBUTTONDOWN',
                'EVENT_MBUTTONDOWN', 'EVENT_LBUTTONUP','EVENT_RBUTTONUP',
@@ -12,7 +10,7 @@ int_to_event = dict(zip((range(len(mouse_event))), mouse_event))
 
 picked_points = []
 def draw_circle(event,x,y,flags,param):
-    print(f'Event : {int_to_event[event]}')
+    # print(f'Event : {int_to_event[event]}')
     if event == cv.EVENT_LBUTTONDOWN:
         picked_points.append( (x, y) )
         cv.circle(img, (x, y), 5, (255, 0, 0), thickness=2)
@@ -24,17 +22,25 @@ def draw_circle(event,x,y,flags,param):
         for p in picked_points:
             pts = np.vstack( (pts, np.array([p[0], p[1]])) )
         pts = pts[1:, :]
-        cv.polylines(img, [pts], True, color=(0, 0, 255))
+        cv.polylines(img, [pts], True, color=(0, 0, 255), thickness=1)
+        pts = pts.astype(np.float32)
+        m = cv.getPerspectiveTransform(pts, dstPts)
+        dst = cv.warpPerspective(img,m,(img.shape[0],img.shape[1]))
+        cv.imshow('dst image',dst)
 
 
-img = np.zeros((512,512,3), np.uint8)
-cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
-while(1):
-    cv.imshow('image',img)
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 27:
-        break
-cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    img = cv.imread(path + 'road.jpg')
+    dstPts = np.array([ [0, img.shape[1]], [0,0], [img.shape[0], 0],[img.shape[0], img.shape[1]] ],np.float32)
+    print(dstPts)
+    cv.namedWindow('image')
+    cv.setMouseCallback('image',draw_circle)
+    while(1):
+        cv.imshow('image',img)
+        k = cv.waitKey(1) & 0xFF
+        if k == ord('m'):
+            mode = not mode
+        elif k == 27:
+            break
+    cv.destroyAllWindows()
